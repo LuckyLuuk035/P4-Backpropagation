0ad888b42a8fbbe20bf623b5d4a49e1f2e8d003e
LuckyLuuk035 <luckyluuk035@icloud.com>
2022-03-03 15:00:51 +0100

    Update Neuron.py

--- Neuron.py
+++ Neuron.py
@@ -6,6 +6,7 @@ class Neuron:
         self.name = name
         self.b = 0
         self.e = 2.718281828459045
+        self.cost = 0
         self.msg = -1
         if w is None:
             w = []
@@ -17,28 +18,31 @@ class Neuron:
     def update(self, truthtable, epochs=25, show=False):
         for i in range(epochs):
             for row in truthtable:
-                d = row[0]
-                x = row[1]
-                y = self.activate(x)  # output
+                y = row[0]  # true value
+                x = row[1]  # input
+                a = self.activate(x)  # output
 
-                self.msg +="    t:   " + str(d)
+                e = y - a  # error
+                self.cost += e**2
+
+                CtoAl = 2 * (a - y)
+                AltoZl =
+
+                self.msg += "    t:   " + str(y)
                 if show:
                     print(self)
                 elif i + 1 == epochs:
                     print(self)
 
-                e = d - y
-                for j in range(len(self.w)):
-                    self.w[j] += 0.2 * e * x[j]
-                self.b += 0.2 * e
+        self.cost = self.cost / epochs
+        print(self.cost)
 
     def activate(self, event):
         self.msg = ""
-        som = 0
+        som = 0  # z
         for i in range(len(self.w)):
             self.msg += "'" + str(event[i]) + "i " + str(self.w[i]) + "w" + "'  "
-            som = som + event[i]*self.w[i]
+            som = som + event[i] * self.w[i]
         self.msg += str(self.b) + "b  "
-        sigmoid_function = 1 / (1 + self.e**-(som + self.b))
+        sigmoid_function = 1 / (1 + self.e ** -(som + self.b))  # a
         return sigmoid_function
-
