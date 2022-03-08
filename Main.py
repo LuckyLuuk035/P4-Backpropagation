cacd7710b1c6a23d522c945553a67e113fbbf836
LuckyLuuk0351 <luuk.bouwman@student.hu.nl>
2022-03-08 21:52:22 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,16 +5,11 @@ import random
 
 
 # onderstaande gegevens zijn op basis van de uitwerkingen van les 6.
-# m = Neuron([1, 0], 0)
-# n = Neuron([0, 1], 0)
+o = Neuron([1, 1], -1.5)  # [-0.5, 0.5], 1.5
 
-o = Neuron([-0.5, 0.5], 1.5)
-
-# inputLayer = NeuronLayer([m, n])
 outputLater = NeuronLayer([o])
 
-andGate = NeuronNetwork([outputLater])  # inputLayer,
-
+andGate = NeuronNetwork([outputLater])
 andGate.feed_forward([0, 0])
 
 f = Neuron([0.2, -0.4], 0)
