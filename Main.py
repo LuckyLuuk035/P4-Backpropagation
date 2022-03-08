a4f70475d05483aca419cfe300749b870fc800ca
LuckyLuuk0351 <luuk.bouwman@student.hu.nl>
2022-03-08 21:48:18 +0100

    added XOR

--- Main.py
+++ Main.py
@@ -5,16 +5,25 @@ import random
 
 
 # onderstaande gegevens zijn op basis van de uitwerkingen van les 6.
-m = Neuron([1, 0], 0)
-n = Neuron([0, 1], 0)
+# m = Neuron([1, 0], 0)
+# n = Neuron([0, 1], 0)
 
 o = Neuron([-0.5, 0.5], 1.5)
 
-inputLayer = NeuronLayer([m, n])
+# inputLayer = NeuronLayer([m, n])
 outputLater = NeuronLayer([o])
 
-andGate = NeuronNetwork([inputLayer, outputLater])
+andGate = NeuronNetwork([outputLater])  # inputLayer,
 
 andGate.feed_forward([0, 0])
 
+f = Neuron([0.2, -0.4], 0)
+g = Neuron([0.7, 0.1], 0)
+hiddenLayer = NeuronLayer([f, g])
 
+o = Neuron([0.6, 0.9], 0)
+outputLater = NeuronLayer([o])
+
+xorGate = NeuronNetwork([hiddenLayer, outputLater])
+
+xorGate.feed_forward([0, 0])
--- NeuronNetwork.py
+++ NeuronNetwork.py
@@ -14,18 +14,16 @@ class NeuronNetwork:
 
     def feed_forward(self, event=None):
         if event:
-            # This only happens on the input layer. (activates layer with input)
+            # This only happens on the first layer. (activates layer with input)
             self.layers[self.count].activate(event)
         else:
-            # Activate layer with the output from the last layer.
-            print(list(self.layers[self.count-1].neurons.values()))
+            # Activate layer with the output from the layer before it.
             self.layers[self.count].activate(list(self.layers[self.count-1].neurons.values()))
         self.count += 1
         if self.count < len(self.layers):
             self.feed_forward()
         else:
             self.count = 0
-            print("we made it to the end!")
             print(self)
 
 
