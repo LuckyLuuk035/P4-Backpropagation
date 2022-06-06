f29e592e43b163f893f9dd0b60c485baed5caa42
LuckyLuuk035 <luckyluuk035@icloud.com>
2022-06-06 21:39:50 +0200

    Update NeuronLayer.py

--- NeuronLayer.py
+++ NeuronLayer.py
@@ -1,5 +1,8 @@
+from Neuron import Neuron
+
+
 class NeuronLayer:
-    def __init__(self, neurons):
+    def __init__(self, neurons=None):
         self.neurons = neurons
 
     def __str__(self):
@@ -12,6 +15,10 @@ class NeuronLayer:
     def activate(self, event):
         for n in self.neurons:
             n.activate(event)
+
+    def addNeurons(self, amount):
+        for i in range(amount):
+            self.neurons += Neuron([0, 0], 0)
 
     def getOutput(self):
         return [i.output for i in self.neurons]
